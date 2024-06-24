f5cc7903bcedcc6be7e9b7ad524bb9317d63546a
DevanshTomar <tomardevansh83@gmail.com>
2024-06-23 19:09:16 -0700

    imporving turning and reversing controls and adding modularity

--- car.js
+++ car.js
@@ -1,60 +1,74 @@
-class Car{
-    constructor(x, y, width, height){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
-        this.speed = 0;
-        this.acceleration = 0.2;
-        this.maxSpeed = 3;
-        this.friction = 0.05
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+    this.angle = 0;
 
-        this.controls = new Controls();
+    this.controls = new Controls();
+  }
+
+  update() {
+    this.#applyControls();
+    this.#applyFriction();
+    this.#updatePosition();
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+    ctx.beginPath();
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.fill();
+    ctx.restore();
+  }
+
+  #applyControls() {
+    if (this.controls.up) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.down) {
+      this.speed -= this.acceleration;
+    }
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
     }
 
-    update(){
-        if(this.controls.up){
-            this.speed += this.acceleration;
-        }
-        if(this.controls.down){
-            this.speed -= this.acceleration;
-        }
-        if(this.speed> this.maxSpeed){
-            this.speed = this.maxSpeed;
-        }
-        if(this.speed < -this.maxSpeed/2){
-            this.speed = -this.maxSpeed/2;
-        }
-        if(this.speed > 0){
-            this.speed -= this.friction;
-        }
-        if(this.speed < 0){
-            this.speed += this.friction;
-        }
-        if(Math.abs(this.speed) < this.friction){
-            this.speed = 0;
-        }
-
-        if(this.controls.left){
-            this.x -= this.speed;
-        }
-        if(this.controls.right){
-            this.x += this.speed;
-        }
-        if(this.controls.left){
-            this.x -= 2;
-        }
-        if(this.controls.right){
-            this.x += 2;
-        }
-        this.y -= this.speed;
-    
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
     }
+  }
 
-    draw(ctx){
-        ctx.beginPath();
-        ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
-        ctx.fill();
+  #applyFriction() {
+    if (this.speed > 0) {
+      this.speed -= this.friction;
     }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+  }
+
+  #updatePosition() {
+    this.x -= this.speed * Math.sin(this.angle);
+    this.y -= this.speed * Math.cos(this.angle);
+  }
 }
