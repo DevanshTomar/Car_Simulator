2f868648e2cc26129c306af13f37a4fe42a50934
DevanshTomar <tomardevansh83@gmail.com>
2024-06-23 18:51:35 -0700

    imporved car controls

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const canvas = document.getElementById('myCanvas');
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
 
-const car = new Car(100, 100, 30, 30);
-car.draw(ctx);
+const car = new Car(100, 100, 30, 50);
+
+animate();
+
+function animate(){
+    canvas.height = window.innerHeight;
+    car.update();
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
